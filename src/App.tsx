886da3f9aa9745999316e4f0b85c7f7281b9109a
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-03-31 21:08:27 -0300

    md app - se agrega el provider de Material, se generan las rutas del proyecto, se agrega el loading y scrollTotop, se establece el layout en las rutas del proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./modules/home/page/HomePage";
+import { MUIThemeProvider } from "./theme/ThemeProvider";
+import { Suspense } from "react";
+import { Layout } from "./shared/Layout/page/Layout";
+import ScrollToTop from "./shared/components/ScrollToTop";
+import Loading from "./shared/components/Loading";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <MUIThemeProvider>
+      <BrowserRouter>
 
-export default App
+        <Suspense fallback={<Loading />}>
+          <ScrollToTop />
+            <Routes>
+              {/* Routes with Layout */}
+              <Route element={<Layout></Layout>}>
+                <Route path="/" element={<HomePage />} />
+                {/* <Route path="/melena" element={<MelenaPage />} />
+                <Route path="/cordyceps" element={<CordycepsPage />} />
+                <Route path="/reishi" element={<ReishiPage />} />
+                <Route path="/cola-pavo" element={<ColaPavoPage />} />
+                <Route path="/preguntas-frecuentes" element={<FAQPage />} />
+                <Route path="/contacto" element={<ContactPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/404" element={<PageNotFound />} /> */}
+
+                {/*Not found or error
+                <Route path="*" element={<Navigate to="/404" replace />} /> */}
+              </Route>
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </MUIThemeProvider>
+  );
+}

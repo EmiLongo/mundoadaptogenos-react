a64d6aeed30b045eb81b165b75fa58e0d05f4268
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-03-31 21:04:21 -0300

    add layout -se crea un layout basico

---
+++ src/shared/Layout/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
---
+++ src/shared/Layout/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>Home Page</h1>
+    </header>
+  );
+};

36a07ec8ae69165923d35a7f440c3eb3fd22948f
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-04-26 18:47:45 -0300

    minor change

--- src/shared/Layout/page/Layout.tsx
+++ src/shared/Layout/page/Layout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import React from "react";
 
-export const Layout = () => {
+export const Layout: React.FC = () => {
   return (
     <>
       <Header />

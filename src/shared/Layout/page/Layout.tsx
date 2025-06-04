34cdd6f6ab21879ef1bca211a68733dd968a37de
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-06-04 19:13:20 -0300

    md layout - se agrega la etiqueta main al oulet, se agrega el boton de WApp y la notificacion

--- src/shared/Layout/page/Layout.tsx
+++ src/shared/Layout/page/Layout.tsx
@@ -1,14 +1,37 @@
+// src/shared/Layout/page/Layout.tsx
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import React from "react";
+import { Box } from "@mui/material";
+import { WhatsApp } from "../components/WhatsApp";
+import { Bounce, ToastContainer } from "react-toastify";
 
 export const Layout: React.FC = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Box 
+      component={"main"}
+      sx={{ flexGrow: 1 }}
+      >
+        <Outlet />
+      </Box>
       <Footer />
+      <WhatsApp />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </>
   );
 };

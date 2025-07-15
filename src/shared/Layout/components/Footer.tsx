3b9011b0b54d8bfa6629134aca653e2f2ec6ba39
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-15 11:00:41 -0300

    md Footer - se actualizan imgs, datos y se estiliza como figma

--- src/shared/Layout/components/Footer.tsx
+++ src/shared/Layout/components/Footer.tsx
@@ -5,8 +5,8 @@ import {
   Container,
 } from "@mui/material";
 
-// import inpulseLogo from "@img/inpulse_design_logo_blanco.svg";
-import logoContraste from "@img/logo-nombre-vertical.svg";
+import inpulseLogo from "@img/inpulse_design_logo_blanco.svg";
+import logoContraste from "@img/logo-nombre-vertical-claro.svg";
 import { menuItems, productsItems } from "../utils/info";
 import {
   BodyS,
@@ -14,13 +14,12 @@ import {
   Heading5,
 } from "@/theme/textStyles";
 import { toast } from "react-toastify";
-import { greyColor } from "@/theme/theme";
+import { brownColor, greyColor } from "@/theme/theme";
 import { OnlyTextButton } from "@/shared/components/buttons/OnlyTextButton";
 import { useNavigate } from "react-router-dom";
-import { NewsletterForm } from "./NewsletterForm";
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@img/icons/RiWhatsappFill.svg';
+import InstagramIcon from '@img/icons/RiInstagramFill.svg';
+import FacebookIcon from '@img/icons/RiFacebookCircleFill.svg';
 
 export const Footer: React.FC = () => {
 
@@ -49,40 +48,39 @@ export const Footer: React.FC = () => {
       id="footer" 
       component="footer" 
       sx={{
-        backgroundColor: "background.paper",
+        backgroundColor: brownColor[950],
         color: greyColor[950], 
         borderTop: `1px ${greyColor[400]} solid`,
         paddingTop: "24px"
       }}
     >
-      <Container sx={{maxWidth: {xs: "unset", sm:"550px", lg:"950px"}}}>
+      <Container sx={{maxWidth: {xs: "unset", sm:"470px", lg:"950px"}, paddingBottom: "20px"}}>
         <Box
           id="footerContainer"
           sx={{
             width: "100%",
             display: "grid",
-            // justifyContent: "space-between",
-            gridTemplateColumns: {xs: "repeat(2, auto)", sm: "repeat(2, auto)", lg: "1fr repeat(2, 2fr) 4fr;"},
-            gridTemplateRows: {xs:"repeat(5, auto)", sm: "repeat(3, auto)", lg: "repeat(2, auto)"},
-            // gap: "auto", 
-            columnGap: "2.5rem",
-            // gridColumnGap: "0px",
-            rowGap: {xs: "20px", sm:"20px", lg:"0px"},
+            gridTemplateColumns: {xs: "repeat(2, auto)", sm: "160px 1fr", lg: "1fr 2fr 3fr"},
+            gridTemplateRows: {xs:"repeat(4, auto)", sm: "repeat(3, auto)", lg: "repeat(2, auto)"},
+            columnGap: {xs: "30px", sm: "2.5rem"},
+            rowGap: {xs: "10px", sm:"20px", lg:"0px"},
           }}
         >
           {/* Logo */}
-          <Box
-            component={"img"}
-            src={logoContraste}
-            alt="Óptica Villagra Blanco y Negro"
-            decoding="async"
-            loading="lazy"
-            onClick={shareURL}
-            sx={{
-              width: "70px",
-              gridArea: {xs:"2 / 1 / 3 / 2", sm:"1 / 1 / 2 / 2", lg:"1 / 1 / 3 / 2"},
-            }}
-          />
+          <Box sx={{display: "flex", justifyContent: {xs:"center", sm:"start",}, alignContent: "start"}}>
+            <Box
+              component={"img"}
+              width="80px"
+              src={logoContraste}
+              alt="Logo Claro Mundo Adaptógenos"
+              decoding="async"
+              loading="lazy"
+              onClick={shareURL}
+              sx={{
+                gridArea: {xs:"1 / 1 / 2 / 2", sm:"1 / 1 / 2 / 2", lg:"1 / 1 / 2 / 2"},
+              }}
+            />
+          </Box>
 
           {/* Categorías */}
           <Box
@@ -90,41 +88,48 @@ export const Footer: React.FC = () => {
               display: "flex",
               flexDirection: "column",
               gap: "8px",
-              gridArea: {xs: "4 / 1 / 5 / 3", sm: "2 / 1 / 3 / 2", lg:"1 / 2 / 2 / 3"},
+              gridArea: {xs: "3 / 1 / 4 / 3", sm: "2 / 1 / 3 / 3", lg:"1 / 3 / 2 / 4"},
             }}
           >
             <Heading5 
             sx={{
-              width: {xs: "100%", md: "unset"},
-              textAlign: {xs: "center", md: "left"}
+              width: "100%",
+              textAlign: {xs: "center", lg: "left"},
+              color: greyColor[50],
             }}>
               Categorías</Heading5>
-            <List sx={{display: "flex", flexDirection: {xs: "row", sm:"column"}, flexWrap: {xs: "wrap", md: "nowrap"}, gap:"8px"}}>
-              {productsItems.map((item) => (
-                <Box key={item.text.replace(" ","-")} component={"a"} href={item.path}>
-                  <BodyS sx={{ width: {xs: "45vw", md: "unset"}, color: greyColor[800], "&:hover":{color: "text.primary"}}}>
-                    {item.text}
-                  </BodyS>
-                </Box>
-              ))}
-            </List>
-          </Box>
+            <Box sx={{display: "flex", gap: {xs:"unset", sm:"40px", lg:"20px"}, justifyContent: {xs: "center", sm: "start"}}}>
+              <List sx={{
+                display: "flex", 
+                flexDirection: "column", 
+                // flexWrap: {xs: "wrap", md: "nowrap"}, 
+                gap:"8px"
+              }}>
+                {productsItems.map((item) => (
+                  <Box key={item.text.replace(" ","-")} component={"a"} href={item.path}>
+                    <BodyS sx={{ width: {xs: "37vw", sm: "160px", lg:"unset"}, color: greyColor[300], "&:hover":{color: brownColor[400]}}}>
+                      {item.text}
+                    </BodyS>
+                  </Box>
+                ))}
+              </List>
+              {/* Secciones de páginas */}
+              <List sx={{
+                display: "flex", 
+                flexDirection: "column", 
+                // flexWrap: {xs: "wrap", md: "nowrap"}, 
+                gap:"8px"
+              }}>
+                {menuItems.map((item) => (
+                  <Box key={item.text.replace(" ","-")} component={"a"} href={item.path}>
+                    <BodyS sx={{ width: {xs: "37vw", sm: "unset"}, color: greyColor[300], "&:hover":{color: brownColor[400]}}}>
+                      {item.text}
+                    </BodyS>
+                  </Box>
+                ))}
+              </List>
 
-          {/* Secciones de páginas */}
-          <Box sx={{
-            gridArea: {xs: "5 / 1 / 6 / 3", sm:"3 / 1 / 4 / 2" , lg: "2 / 2 / 3 / 3"},
-            display: {xs: "flex", sm:"block"},
-            justifyContent: "space-between"
-          }}>
-            <List sx={{display: "flex", flexDirection: {xs: "row", sm:"column"}, flexWrap: {xs: "wrap", md: "nowrap"}, gap:"8px"}}>
-              {menuItems.map((item) => (
-                <Box key={item.text.replace(" ","-")} component={"a"} href={item.path}>
-                  <BodyS sx={{ width: {xs: "45vw", md: "unset"}, color: greyColor[800], "&:hover":{color: "text.primary"}}}>
-                    {item.text}
-                  </BodyS>
-                </Box>
-              ))}
-            </List>
+            </Box>
           </Box>
 
           {/* Contacto */}
@@ -132,27 +137,27 @@ export const Footer: React.FC = () => {
             sx={{
               display: "flex",
               flexDirection: "column",
-              alignItems: {xs: "start", sm: "center", lg: "start"},
+              alignItems: {xs: "start", sm: "start", lg: "start"},
               gap: "8px",
-              gridArea: {xs: "2 / 2 / 4 / 3", sm: "2 / 2 / 3 / 3" , lg: "1 / 3 / 2 / 4"},
+              gridArea: {xs: "1 / 2 / 3 / 3", sm: "1 / 2 / 2 / 3" , lg: "1 / 2 / 2 / 3"},
             }}
           >
-            <Heading5 sx={{width: {xs: "unset",sm: "210px", lg:"unset"},}}>Contáctanos</Heading5>
+            <Heading5 sx={{width: {xs: "unset",sm: "210px", lg:"unset"}, color: greyColor[50],}}>Contáctanos</Heading5>
             <List sx={{width: {xs: "unset",sm: "210px", lg:"unset"},}}>
-              <BodyS sx={{ color: greyColor[800]}}>
-                Mitre 1745, S. C. de Bariloche.
+              <BodyS sx={{ color: greyColor[300]}}>
+              Sadi Carnot 5952, Rosario.
               </BodyS>
-              <BodyS sx={{ color: greyColor[800]}}>
-                Rio Negro, Argentina.
+              <BodyS sx={{ color: greyColor[300]}}>
+              Santa Fe, Argentina.
               </BodyS>
-              <BodyS sx={{ color: greyColor[800]}}>
-                CP 8400
+              <BodyS sx={{ color: greyColor[300]}}>
+              CP 2000
               </BodyS>
-              <BodyS sx={{ color: greyColor[800]}}>
-                +54 9 11 6298 4904
+              <BodyS sx={{ color: greyColor[300]}}>
+              +54 9 341 266-7096
               </BodyS>
-              <BodyS sx={{ color: greyColor[800]}}>
-                info@kostentrek.com
+              <BodyS sx={{ color: greyColor[300]}}>
+              mundoadaptogenos@gmail.com
               </BodyS>
             </List>
           </Box>
@@ -160,111 +165,92 @@ export const Footer: React.FC = () => {
           {/* Redes Sociales */}
           <Box sx={{
             display:"flex", 
-            justifyContent: {xs: "start", sm: "center", lg: "start"},
+            justifyContent: {xs: "center", sm: "center", lg: "start"},
           }}>
             <Box sx={{
               display:"flex", 
               width: {xs: "unset",sm: "210px", lg:"unset"},
               gap: "8px", 
-              gridArea: {xs: "3 / 1 / 4 / 2", sm: "3 / 2 / 4 / 3" , lg: "2 / 3 / 3 / 4"},
+              gridArea: {xs: "2 / 1 / 3 / 2", sm: "3 / 1 / 4 / 2" , lg: "2 / 2 / 3 / 3"},
             }}>
-            <WhatsAppIcon width={20} sx={{color: "primary.main"}} />
-            <InstagramIcon width={20} sx={{color: "primary.main"}} />
-            <FacebookIcon width={20} sx={{color: "primary.main"}} />
+            <Box component={"img"} src={WhatsAppIcon} width="24px" sx={{color: "primary.main"}} />
+            <Box component={"img"} src={InstagramIcon} width="24px" sx={{color: "primary.main"}} />
+            <Box component={"img"} src={FacebookIcon} width="24px" sx={{color: "primary.main"}} />
             </Box>
           </Box>
 
-          {/* Newsletter */}
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "8px",
-              gridArea: {xs: "1 / 1 / 2 / 3", sm: "1 / 2 / 2 / 3" , lg: "1 / 4 / 3 / 5"},
-            }}
-          >
-            <Heading5 sx={{textWrap: "nowrap"}}>Recibí novedades y enterate de las ofertas!</Heading5>
-            <NewsletterForm />
+          {/* Términos y Condiciones */}
+          <Box sx={{
+            gridArea: {xs: "4 / 1 / 5 / 3", sm: "3 / 2 / 4 / 3" , lg: "2 / 3 / 3 / 4"},
+            display: "flex",
+            flexDirection: {xs: "column", sm: "row"},
+            gap: {xs:"8px", sm:"16px", lg:"24px"},
+          }}>
+            <OnlyTextButton
+              id="bt-footer-terms-conditions"
+              onClick={()=>navigate("./terms-and-conditions")}
+              type="primaryLigthButton"
+              text="Términos y condiciones"
+              isFetching={false}
+              disabled={false}
+              sx={{
+                textWrap: "nowrap",
+                marginX: {xs: "auto", sm:"unset"}
+              }}
+            />
+            {/* <OnlyTextButton
+              id="bt-footer-privacy-policy"
+              onClick={()=>navigate("./privacy-policy")}
+              type="primaryLigthButton"
+              text="Políticas de privacidad"
+              isFetching={false}
+              disabled={false}
+              sx={{
+                textWrap: "nowrap"
+                }}
+                /> */}
           </Box>
 
-          
-          {/* <Divider
-          component="hr"
-          variant="fullWidth"
-          aria-hidden="true"
-          sx={{ 
-            marginTop: "2rem", 
-            marginBottom: "1rem", 
-            backgroundColor: palette.primary[100] 
-          }} 
-          /> */}
         </Box>
         
 
-          <Box
-            id="footerBottom"
-          >
 
-            {/* Términos y Condiciones */}
-            <Box sx={{
-              display: "flex",
-              flexDirection: {xs: "column", md: "row"},
-              justifyContent: "center",
-              alignItems: {xs: "center", md: "start"},
-              gap: {xs:"8px", sm:"16px", lg:"24px"}
-            }}>
-              <OnlyTextButton
-                id="bt-footer-terms-conditions"
-                onClick={()=>navigate("./terms-and-conditions")}
-                type="primaryButton"
-                text="Términos y condiciones"
-                isFetching={false}
-                disabled={false}
-                sx={{
-                  textWrap: "nowrap"
-                }}
-              />
-              <OnlyTextButton
-                id="bt-footer-privacy-policy"
-                onClick={()=>navigate("./privacy-policy")}
-                type="primaryButton"
-                text="Políticas de privacidad"
-                isFetching={false}
-                disabled={false}
-                sx={{
-                  textWrap: "nowrap"
-                }}
-              />
-            </Box>
-        {/* service Mark y Trade Mark ℠™ */}
-            <CaptionAlt sx={{ color: "inherit", textAlign: "center", marginY: "1rem" }}>
-              {new Date().getFullYear()} | TM
-            </CaptionAlt>
-            {/* <Box
-              component={"a"}
-              href="https://inpulse.com.ar"
-              target="_blank"
-              rel="noopener noreferrer"
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                gap: "0.75rem",
-              }}
-            >
-              <BodyM sx={{ color: "inherit",textAlign: "center" }}>
-                Desarrollado por
-              </BodyM>
-              <Box
-                component={"img"}
-                src={inpulseLogo}
-                alt="Logo Inpulse Design"
-                width={100}
-                decoding="async"
-                loading="lazy"
-              />
-            </Box> */}
-                      </Box>
       </Container>
+          {/* service Mark y Trade Mark ℠™ */}
+      <Box
+        id="footerBottom"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "12px", 
+          backgroundColor: brownColor[900], 
+          color: greyColor[200],
+        }}
+      >
+        <CaptionAlt sx={{ color: "inherit", textAlign: "center", marginY: "1rem" }}>
+          {new Date().getFullYear()} ™ | Desarrollado por
+        </CaptionAlt>
+        <Box
+          component={"a"}
+          href="https://inpulse.com.ar"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.75rem",
+          }}
+        >
+          <Box
+            component={"img"}
+            src={inpulseLogo}
+            alt="Logo Inpulse Design"
+            height={20}
+            decoding="async"
+            loading="lazy"
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };

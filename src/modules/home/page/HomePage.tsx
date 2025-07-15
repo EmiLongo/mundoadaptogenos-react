45107c240bacd0108a5376426590065f462b82b7
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-15 11:35:58 -0300

    se quitan las secciones de la optica

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,13 +1,11 @@
 // src/modules/home/page/HomePage.tsx
 import { Hero } from "../components/Hero";
 import { Highlights } from "../components/Highlights";
-import { Sections } from "../components/Sections";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
-      <Sections />
       <Highlights />
     </>
   );

ed3b8dbac3ef23a82acba2744ff5ab3679916fb2
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-16 16:22:19 -0300

    md HomePage - se agrega PresentationPower

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,11 +1,13 @@
 // src/modules/home/page/HomePage.tsx
 import { Hero } from "../components/Hero";
 import { Highlights } from "../components/Highlights";
+import { PresentationPower } from "../components/PresentationPower";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
+      <PresentationPower />
       <Highlights />
     </>
   );

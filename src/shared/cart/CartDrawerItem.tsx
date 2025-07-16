2cc423c4d70c3d294bedb48633fe33417e26fe3f
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-16 20:14:50 -0300

    md CartDrawerItem - se trae la funcionalidad de delete aqui, se quita el modal, se agrega boton y fondo de img espejado

--- src/shared/cart/CartDrawerItem.tsx
+++ src/shared/cart/CartDrawerItem.tsx
@@ -1,13 +1,15 @@
 // src/shared/cart/CartDrawerItem.tsx
 import { Box } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ICartItem } from "./types";
-import { BodyMEmph, BodyS } from "@theme/textStyles";
+import { BodyMEmph, BodyS, ButtonS } from "@theme/textStyles";
 import { numberToPrice } from "@shared/utils/convertNumberToPrice";
 import { WhiteButton } from "@shared/components/buttons/WhiteButton";
 import { ProductCounter } from "./ProductCounter";
 import { useCart } from "@store/useCartStore";
-import { ModalDeleteCartItem } from "./ModalDeleteCartItem";
+import { toast } from "react-toastify";
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import { greyColor, redColor } from "@/theme/theme";
 
 interface ICartDrawerItem {
   cartItem: ICartItem;
@@ -15,9 +17,11 @@ interface ICartDrawerItem {
   closeCartDrawer: ()=>void;
 }
 export const CartDrawerItem: React.FC<ICartDrawerItem> = ({cartItem, index, closeCartDrawer}) => {
-  const { updateProductQuantity, isLoading } = useCart();
+  const { updateProductQuantity, removeProduct, error, isLoading, clearError } = useCart();
   const [counter, setCounter] = useState<number>(cartItem.quantity);
-  const [openDeleteItemModal, setOpenDeleteItemModal] = useState<boolean>(false);
+
+  const [showDeleteBt, setShowDeleteBt] = useState<boolean>(false)
+  const [isFetching, setIsFetching] = useState<boolean>(false)
 
   const handleAdd = () => {
     const quantity = counter+1;
@@ -31,29 +35,119 @@ export const CartDrawerItem: React.FC<ICartDrawerItem> = ({cartItem, index, clos
     updateProductQuantity(cartItem.productId, quantity)
   }
 
-  const handleDeleteCartItem = () =>{
-    setOpenDeleteItemModal(true)
+  const handleShowDelete = () =>{
+    setShowDeleteBt(!showDeleteBt)
   }
 
-  const handleToogleDeleteItemModal = () => {
-    setOpenDeleteItemModal(!openDeleteItemModal)
+  const handleDeleteCartItem = () => {
+    setIsFetching(true)
+    removeProduct(cartItem.productId);
   }
+
+  useEffect(() => {
+    if(error){
+    toast.error("Error: No se pudo borrar el Item")
+    const timeout = setTimeout(() => {
+      clearError();
+    }, 5000);
+    return () => clearTimeout(timeout); // Limpia si se desmonta antes
+    }
+  }, [error])
+
+  useEffect(() => {
+    if (isFetching && !isLoading) {
+      setIsFetching(false)
+      closeCartDrawer();
+    }
+  }, [isFetching])
 
   return (
     <Box 
     key={`cart-item-${index}`}
-    sx={{display: "flex", alignItems: "center", gap: "1rem", marginBottom: "16px"}}
-    >
+    sx={{
+      width: showDeleteBt ? "130%" : "100%",
+      display: "flex", 
+      alignItems: "center", 
+      marginBottom: "16px", 
+      overflowX: "hidden",
+      gap: 0,
+    }}
+    > 
+      <Box 
+        sx={{
+          display: "flex",
+          justifyContent: "center", 
+          alignItems: "center", 
+          height: "120px",
+          width: showDeleteBt ? "95px" : 0,
+          opacity: showDeleteBt ? 1 : 0,
+          position: "relative",
+          transition: "all 0.3s ease-in-out",
+
+        }}
+      >
+        <Box component="img" width="100px" height="120px"
+        alt={`Foto del Producto ${index} del Carrito`}
+        src={cartItem.product.urlThumbnail}
+        sx={{
+          borderTopRightRadius: "18px", 
+          borderBottomRightRadius: "18px", 
+          transform: "scaleX(-1)",
+          position: "absolute",
+          zIndex:2,
+        }}
+        />
+        <Box 
+          onClick={handleDeleteCartItem}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center", 
+            alignItems: "center", 
+            gap: "4px",
+            color: greyColor[50],
+            width: "95px",
+            height: "120px",
+            background: redColor[700],
+            borderRadius: "8px",
+            zIndex: 3,
+            cursor: "pointer",
+            "&:hover":{
+              background: redColor[800],
+            }
+          }}
+        >
+          <ButtonS sx={{color: "inherit"}}>BORRAR</ButtonS>
+          <DeleteForeverOutlinedIcon width="24px" />
+        </Box>
+      </Box>
       <Box component="img" width={120} height={120}
       alt={`Foto del Producto ${index} del Carrito`}
       src={cartItem.product.urlThumbnail}
-      sx={{borderRadius: "8px"}}
+      sx={{
+                  border: "none",
+        borderRadius: "8px",
+        borderTopLeftRadius: showDeleteBt ? 0 : "8px",
+        borderBottomLeftRadius: showDeleteBt ? 0 : "8px",
+        marginRight: "16px"
+        
+      }}
       />
       <Box sx={{display: "flex", flexDirection: "column", gap: "8px"}}>
         <BodyS sx={{height: "2.5em"}}>{cartItem.product.title}</BodyS>
         <BodyMEmph>{numberToPrice(cartItem.product.price * counter)}</BodyMEmph>
         <Box sx={{display: "flex", alignItems: "center", gap: "8px"}}>
-          <ProductCounter 
+        {showDeleteBt
+        ? <WhiteButton
+          id={`delete-cart-item-${index}`}
+          text="Cancelar"
+          onClick={handleShowDelete}
+          fetchingText={"borrando"}
+          isFetching={false}
+          disabled={isLoading}
+          sx={{width: "150px"}}
+          />
+        : <><ProductCounter 
           index={index}
           counter={counter} 
           handleAdd={handleAdd}
@@ -62,22 +156,14 @@ export const CartDrawerItem: React.FC<ICartDrawerItem> = ({cartItem, index, clos
           <WhiteButton
           id={`delete-cart-item-${index}`}
           text="BORRAR"
-          onClick={() => handleDeleteCartItem()}
+          onClick={handleShowDelete}
           fetchingText={"borrando"}
           isFetching={false}
           disabled={isLoading}
           sx={{width: "85px"}}
-          />
+          /></>}
         </Box>
       </Box>
-      {openDeleteItemModal && 
-        <ModalDeleteCartItem 
-          openDeleteModal={openDeleteItemModal} 
-          closeDeleteItemModal={handleToogleDeleteItemModal} 
-          cartItem={cartItem}
-          closeCartDrawer={closeCartDrawer}  
-        />
-      }
     </Box>
   )
 }

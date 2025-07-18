ce2b7731f7b440d1df9a288582a26346993359fb
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-17 22:34:45 -0300

    fix loading - se actualizan alt de imgs

--- src/shared/components/Loading.tsx
+++ src/shared/components/Loading.tsx
@@ -15,7 +15,7 @@ const Loading = () => {
       }}
     >
 			<CircularProgress size={150} />
-      <Box component="img" src={logo} alt="Logo Óptica Villagra" width={100}
+      <Box component="img" src={logo} alt="Logo Mundo Adaptógenos" width={100}
         sx={{ 
           position: "absolute", 
           top: "50%", 

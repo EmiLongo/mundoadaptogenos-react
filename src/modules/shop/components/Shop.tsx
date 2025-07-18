ca1951142dd19296583c497bd01d9184295e8d69
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-17 22:47:34 -0300

    add Shop - copia a Highlights pero con otro titulo

---
+++ src/modules/shop/components/Shop.tsx
@@ -0,0 +1,27 @@
+// src/modules/home/components/Highlights.tsx
+import { Box } from "@mui/material"
+import React from "react"
+import { SectionHeading } from "@theme/textStyles"
+import { Carousel } from "@/modules/home/components/Carousel"
+
+
+export const Shop: React.FC = () => {
+  return (
+    <Box sx={{
+      marginBottom: "5rem",
+      paddingX: {xs: "1rem", md: "3rem", lg: "4rem"},
+    }}>
+      {/* TODO: titulo a la izquierda cer figma */}
+      <SectionHeading
+      id= "section-highlights"
+      sx={{
+        width: "100%",
+        textAlign: "center",
+      }}
+      >
+        Productos
+      </SectionHeading>
+      <Carousel />
+    </Box>
+  )
+}

c442ed347960c55f11b0c80f0c276511fbc8e001
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-18 17:38:02 -0300

    Layout - se agrega nuevo Header

--- src/shared/Layout/page/Layout.tsx
+++ src/shared/Layout/page/Layout.tsx
@@ -2,21 +2,18 @@
 import { Outlet, useLocation } from "react-router-dom";
 import React, { lazy } from "react";
 const Footer = lazy(() => import("../components/Footer").then(module => ({ default: module.Footer })));
-import { HeaderOneLine } from "../components/HeaderOneLine";
-import { HeaderTwoLines } from "../components/HeaderTwoLines";
 import { Box } from "@mui/material";
 import { WhatsApp } from "../components/WhatsApp";
 import { Bounce, ToastContainer } from "react-toastify";
 import { ScrollToTop } from "@/routes/ScrollToTop";
-import { navBarLines } from "../utils/info";
+import { HeaderThreeLines } from "../components/HeaderThreeLines";
 
 export const Layout: React.FC = () => {
   const { pathname } = useLocation();
   const isAdmin = pathname.includes("/admin/");
   return (
     <>
-      {navBarLines === 2 && <HeaderTwoLines />}
-      {navBarLines === 1 && <HeaderOneLine />}
+      <HeaderThreeLines />
       <Box 
       component={"main"}
       sx={{ flexGrow: 1 }}

8361178cbf2bdd70e7181e8e6ed7a5faf2345ebb
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-21 21:59:03 -0300

    md Hero: se crea el hero con animaciones

--- src/modules/home/components/Hero.tsx
+++ src/modules/home/components/Hero.tsx
@@ -1,38 +1,264 @@
-import { Box, Typography } from "@mui/material"
-import React from "react"
-import heroImageDesktop from '@img/home/hero_desktop.png'
-import heroImageMobile from '@img/home/hero_mobile.png'
+import React from "react";
+import {
+  Box,
+  Typography,
+  keyframes,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import backgroundHeroWebp from "@img/home/hero/fondo.webp";
+// import backgroundHeroPng from "@img/home/hero/fondo.png"
+import floatingMushroomWebp from "@img/home/hero/floating_mushroom.webp";
+import floatingMushroomPng from "@img/home/hero/floating_mushroom.png";
+import shadowMushroomWebp from "@img/home/hero/sombra.webp";
+import shadowMushroomPng from "@img/home/hero/sombra.png";
+import {
+  DisplayHeadingL,
+  DisplayHeadingXXL,
+  Heading1,
+  Heading2,
+} from "@theme/textStyles";
 
+// Definimos la animación keyframe
+const floatUpAnimation = keyframes`
+  0% {
+    transform: translateY(100vh) rotate(25deg);
+    opacity: 0;
+  }
+  80% {
+    transform: translateY(0) rotate(25deg);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(0) rotate(0deg);
+    opacity: 1;
+  }
+`;
 
+// Animación para la sombra
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  60% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
 
+// Animación para el fondo
+// const rotateAnimation = keyframes`
+//   0% {
+//     transform: rotate(-180deg);
+//   }
+//   100% {
+//     transform: rotate(0deg);
+//   }
+// `;
+
+//Animación para la frase xl
+const translateToRightAnimation = keyframes`
+  0% {
+    transform: translateX(-50%);
+    opacity: 0.5
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1
+  }
+`;
+
+//Animación para la frase l
+const translateToLeftAnimation = keyframes`
+  0% {
+    transform: translateX(50%);
+    opacity: 0.5
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1
+  }
+`;
 
 export const Hero: React.FC = () => {
-  return (      
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isTablet = useMediaQuery(theme.breakpoints.between("md", "lg"));
+  return (
     <>
-    {/* titulo para el SEO, no se muestra */}
+      {/* titulo para el SEO, no se muestra */}
       <Typography
-        variant="h1" 
-        component="h1" 
-        sx={{ 
+        variant="h1"
+        component="h1"
+        sx={{
           color: "transparent",
-          fontSize: '0.1rem',
-          position: 'absolute',
-          top: '-100%',
-          left: '-100%',
+          fontSize: "0.1rem",
+          position: "absolute",
+          top: "-100%",
+          left: "-100%",
         }}
       >
-        Venta de Anteojos de Sol, Armazones de Receta, Clip-On, Indestructibles
+        Venta de Extracción Hongos Adaptógenos, Melena de León, Cordyceps
+        Militaris, Reishi, Cola de Pavo, Màxima Pureza
       </Typography>
-      <Box sx={{
-        backgroundImage: {xs: `url(${heroImageMobile})`, md: `url(${heroImageDesktop})`},
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: {xs: '80vh', md: '75vh', lg: '75vh'},
-        width: '100%',
-        marginBottom: {xs: '1rem', md: '4rem'},
-      }}/>
+      <Box
+        sx={{
+          height: { xs: "80vh", md: "75vh", lg: "75vh" },
+          width: "100%",
+          marginBottom: { xs: "1rem", md: "4rem" },
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow:
+            "inset 0px 10px 10px 0px rgba(41, 23, 17, 0.2), inset 0px -10px 10px 0px rgba(41, 23, 17, 0.2)",
+          position: "relative",
+          overflow: "hidden",
+          backgroundImage: `url(${backgroundHeroWebp})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "212px", sm: "253px", md: "278px", xl: "400px" },
+            zIndex: 10,
+            position: "relative",
+            // animación del hongo
+            animation: `${floatUpAnimation} 1.2s ease-out`,
+          }}
+        >
+          <Box
+            component="picture"
+            sx={{
+              width: "100%",
+              zIndex: 10,
+            }}
+          >
+            <source srcSet={floatingMushroomWebp} type="image/webp" />
+            <Box
+              component="img"
+              src={floatingMushroomPng}
+              alt="Hongo Flotando"
+              loading="lazy"
+              decoding="async"
+              width="100%"
+            />
+          </Box>
 
+          <Box
+            sx={{
+              width: { xs: "212px", sm: "253px", md: "278px", xl: "400px" },
+              zIndex: 10,
+              position: "absolute",
+              bottom: "-30%",
+              // Animación para la sombra (aparece después del hongo)
+              animation: `${fadeInAnimation} 1.8s ease-out`,
+            }}
+          >
+            <Box
+              component="picture"
+              sx={{
+                width: "100%",
+                zIndex: 10,
+              }}
+            >
+              <source srcSet={shadowMushroomWebp} type="image/webp" />
+              <Box
+                component="img"
+                src={shadowMushroomPng}
+                alt="Sombra del Hongo"
+                loading="lazy"
+                decoding="async"
+                width="100%"
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            width: { xs: "335px", sm: "545px", md: "670px", lg: "1050px" },
+            maxWidth: "1050px",
+            height: {
+              xs: "400px",
+              sm: "400px",
+              md: "340px",
+              lg: "360px",
+              xl: "400px",
+            },
+            transform: {
+              xs: "translateY(0)",
+              sm: "translateY(40px)",
+              md: "translateY(-5px)",
+              lg: "translateY(0)",
+              xl: "translateY(-40px)",
+            },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            zIndex: 2,
+          }}
+        >
+          <Box sx={{ animation: `${translateToRightAnimation} 1s ease-out` }}>
+            {isMobile ? (
+              <>
+                <Heading1 sx={{ width: "100%", textAlign: "left" }}>
+                  Tu salud integral
+                </Heading1>
+                <Heading1 sx={{ width: "100%", textAlign: "left" }}>
+                  en equilibrio
+                </Heading1>
+              </>
+            ) : isTablet ? (
+              <>
+                <DisplayHeadingL sx={{ width: "100%", textAlign: "left" }}>
+                  Tu salud integral en
+                </DisplayHeadingL>
+                <DisplayHeadingL sx={{ width: "100%", textAlign: "left" }}>
+                  equilibrio
+                </DisplayHeadingL>
+              </>
+            ) : (
+              <>
+                <DisplayHeadingXXL sx={{ width: "100%", textAlign: "left" }}>
+                  Tu salud integral
+                </DisplayHeadingXXL>
+                <DisplayHeadingXXL sx={{ width: "100%", textAlign: "left" }}>
+                  en equilibrio
+                </DisplayHeadingXXL>
+              </>
+            )}
+          </Box>
+          {isMobile || isTablet ? (
+            <Heading2
+              sx={{
+                width: "100%",
+                textAlign: "right",
+                animation: `${translateToLeftAnimation} 1s ease-out`,
+              }}
+            >
+              De manera natural.
+            </Heading2>
+          ) : (
+            <Heading1
+              sx={{
+                width: "100%",
+                textAlign: "right",
+                animation: `${translateToLeftAnimation} 1s ease-out`,
+                paddingRight: {xs: 0, lg:"3rem", xl: 0}
+              }}
+            >
+              De manera natural.
+            </Heading1>
+          )}
+        </Box>
+      </Box>
     </>
-  )
-}
-
+  );
+};

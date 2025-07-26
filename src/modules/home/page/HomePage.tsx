92a78cdbd1a4e42020135376c36e87f316e44c46
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-26 17:43:05 -0300

    minor fix

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,9 +1,10 @@
 // src/modules/home/page/HomePage.tsx
+import React from "react";
 import { Hero } from "../components/Hero";
 import { Highlights } from "../components/Highlights";
 import { PresentationPower } from "../components/PresentationPower";
 
-export const HomePage = () => {
+export const HomePage: React.FC = () => {
   return (
     <>
       <Hero />

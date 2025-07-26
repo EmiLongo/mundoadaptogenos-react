a456416966e13b5dcae6bc4d1b3096069a7b3f85
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-26 18:08:05 -0300

    add MushroomTypePage - pagina para la info de los hongos

---
+++ src/modules/mushroom-type/page/MushroomTypePage.tsx
@@ -0,0 +1,40 @@
+// src/modules/mushroom-type/page/MushroomTypePage.tsx
+import { Box, useMediaQuery, useTheme } from "@mui/material"
+import React from "react"
+import { IMushroomDetails } from "../components/types"
+import { Benefits } from "../components/Benefits";
+import { Bioactive } from "../components/Bioactive";
+import { MushroomTitle } from "../components/MushroomTitle";
+import { GeneralInfo } from "../components/GeneralInfo";
+import { HealthEffectsMobile } from "../components/HealthEffectsMobile";
+import { HealthEffectsDesktop } from "../components/HealthEffectsDesktop";
+
+interface IMushroomTypePage {
+  mushroom: IMushroomDetails;
+}
+export const MushroomTypePage: React.FC<IMushroomTypePage> = ({ mushroom }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  console.log("hongo", mushroom);
+  return (
+    <Box component="main" sx={{width: "100%"}}>
+      <Box component="h1" sx={{color:"transparent", position:"absolute", zIndex: -1}}>Encontrá la información de tu hongo adaptógeno favorito. Melena de León, Reishi, Cordyceps Militaris, Cola de Pavo</Box>
+      {/* titulo */}
+      <MushroomTitle mushroom={mushroom} isMobile={isMobile} isLargeScreen={isLargeScreen} />
+
+      {/* Beneficios */}
+      <Benefits benefits={mushroom.benefits} isMobile={isMobile} />
+
+      {/* Bioactivos y efectos en la salud*/}
+      <Bioactive bioactive={mushroom.bioactive} isMobile={isMobile} />
+      {isMobile
+        ? <HealthEffectsMobile mushroom={mushroom} />
+        : <HealthEffectsDesktop mushroom={mushroom} />
+      }
+
+      {/* Informacion Gral */}
+      <GeneralInfo mushroom={mushroom} isMobile={isMobile} />
+    </Box>
+  )
+}

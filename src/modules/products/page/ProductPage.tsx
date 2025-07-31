00eecd5af6110ab4389a07906ab14aaf2821facf
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-31 09:43:03 -0300

    fix ProductPage

--- src/modules/products/page/ProductPage.tsx
+++ src/modules/products/page/ProductPage.tsx
@@ -2,15 +2,11 @@ import React from "react";
 
 import { catalogue } from "@shared/Layout/utils/catalogue";
 import { Carousel } from "@modules/home/components/Carousel";
-import { IProduct } from "@/shared/components/types";
+
 import { useLocation } from "react-router-dom";
 import { BigCard } from "../components/BigCard";
 
-interface IProductPage {
-  product: IProduct
-}
-
-export const ProductPage: React.FC<IProductPage> = () => {
+export const ProductPage: React.FC = () => {
   const { state } = useLocation();
   const product = state.product;
 

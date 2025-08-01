cc7cdc0f64fae14f075f4bc0820d8fdf373f7afb
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-31 21:18:18 -0300

    add MushroomByBenefits: se crea una util solo con los beneficios de todos los hongos

---
+++ src/modules/mushroom-type/utils/MushroomByBenefits.tsx
@@ -0,0 +1,34 @@
+import { IBenefit } from "../components/types";
+import { infoColaPavo } from "./ColaPavo";
+import { infoCordyceps } from "./Cordyceps";
+import { infoMelena } from "./Melena";
+import { infoReishi } from "./Reishi";
+
+interface IMushroomByBenefits {
+  title: string;
+  productSection: number[];
+  benefits: IBenefit[];
+}
+
+export const MushroomByBenefits: IMushroomByBenefits[] = [
+  {
+    title: infoColaPavo.title,
+    productSection: infoColaPavo.productSection,
+    benefits: infoColaPavo.benefits,
+  },
+  {
+    title: infoCordyceps.title,
+    productSection: infoCordyceps.productSection,
+    benefits: infoCordyceps.benefits,
+  },
+  {
+    title: infoMelena.title,
+    productSection: infoMelena.productSection,
+    benefits: infoMelena.benefits,
+  },
+  {
+    title: infoReishi.title,
+    productSection: infoReishi.productSection,
+    benefits: infoReishi.benefits,
+  },
+]

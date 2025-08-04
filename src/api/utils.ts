dfcddd2452d7b723ac564ff61bf1abf05ad3a0f8
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-03 23:29:40 -0300

    se actualizan las constantes para el cliente de apis

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,4 +1,5 @@
-export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const backendUrl = import.meta.env.VITE_SUPABASE_URL;
+export const backendUrlKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
 export const imagesEndpoint = import.meta.env.VITE_IMAGES_ENDPOINT;

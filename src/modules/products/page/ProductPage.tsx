1fc2d8a0b3885b80a5d2c1a36a863beaebd6c46d
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-05 18:31:22 -0300

    md ProductPage: tiene la logica para mostrar un tipo de beneficio u otro

--- src/modules/products/page/ProductPage.tsx
+++ src/modules/products/page/ProductPage.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useMediaQuery, useTheme } from "@mui/material";
 import { BigCard } from "../components/BigCard";
 import { ProductBenefits } from "../components/ProductBenefits";
 import { CarouselContainer } from "../components/CarouselContainer";
+import { ProductsBenefitMobile } from "../components/ProductsBenefitMobile";
 
 export const ProductPage: React.FC = () => {
   const { state } = useLocation();
   const product = state.product;
-
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
+  console.log("product: ", product)
   return (
     <>
     <BigCard product={product} />
-    <ProductBenefits sectionsID={product.sectionId} />
+    {(isMobile && product.sectionId.length > 1) 
+      ? <ProductsBenefitMobile sectionsID={product.sectionId} />
+      : <ProductBenefits sectionsID={product.sectionId} />}
     <CarouselContainer />
     </>
   )

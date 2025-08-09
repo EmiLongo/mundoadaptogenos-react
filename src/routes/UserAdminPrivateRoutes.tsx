1916a20768039d947a2e4bac245d3e4530697d41
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-09 14:09:20 -0300

    md UserAdminPrivateRoutes:  se usan los hooks del nuevo store

--- src/routes/UserAdminPrivateRoutes.tsx
+++ src/routes/UserAdminPrivateRoutes.tsx
@@ -1,17 +1,17 @@
+// src/routes/UserAdminPrivateRoutes.tsx
 import { useUserStore } from '@/store/useUserStore';
 import { Navigate } from 'react-router-dom';
 
 
 export function UserAdminPrivateRoutes({ children }: { children: React.ReactNode }) {
 
-  const isAuthenticated = useUserStore((state) => state.isAuthenticated());
-  const user = useUserStore((state) => state.user);
+	const { isAuthenticated, isAdmin } = useUserStore();
 
     if ( !isAuthenticated ) {
         return <Navigate to="/login" replace />;
     }
 
-    if ( user?.role !== 'ADMIN' && user?.role !== 'SUPER_ADMIN' ) {
+    if ( !isAdmin ) {
         return <Navigate to="/" replace />;
     }
 

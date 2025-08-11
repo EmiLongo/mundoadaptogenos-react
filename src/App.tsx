691c6aad7c17a572e0c4719945bc19cf9b1de70f
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-10 21:52:02 -0300

    md App: se agrega el wrapper de AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+// src/App.tsx
 import { MUIThemeProvider } from "./theme/ThemeProvider";
 import { router } from "./routes/AppRoutes";
 import { Suspense } from "react";
-import Loading from "./shared/components/Loading";
 import { RouterProvider } from "react-router-dom";
+import { useUserStore } from "@store/useUserStore";
+import { AuthProvider } from "@shared/hooks/AuthProvider";
+import Loading from "@shared/components/Loading";
 
 export const App = () => {
+  const { hasHydrated } = useUserStore();
+
+  // Mostrar loading hasta que el store se haya hidratado
+  if (!hasHydrated) {
+    return <Loading />;
+  }
+
   return (
-    <MUIThemeProvider>        
-      <Suspense fallback={<Loading />}>
-        <RouterProvider router={router} />
-      </Suspense>
+    <MUIThemeProvider>
+      <AuthProvider>
+        <Suspense fallback={<Loading />}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </AuthProvider>
     </MUIThemeProvider>
   );
 }

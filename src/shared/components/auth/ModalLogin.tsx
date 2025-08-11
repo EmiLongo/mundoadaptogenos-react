6e30f1daa5432e06ca4f881d6d1976b225010ddd
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-11 14:01:22 -0300

    md ModalLogin: estilos como figma

--- src/shared/components/auth/ModalLogin.tsx
+++ src/shared/components/auth/ModalLogin.tsx
@@ -1,9 +1,22 @@
 
-import { Box, IconButton, Modal } from "@mui/material";
+import { Box, IconButton, keyframes, Modal } from "@mui/material";
 import { Login } from "./Login";
 import { Heading3 } from "@/theme/textStyles";
 import { ForgetPass } from "./ForgetPass";
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import { Closebutton } from "../buttons/Closebutton";
+import { heightForModals } from "@/shared/Layout/utils/info";
+import { greyColor, paddingModal } from "@/theme/theme";
+
+// Animación para el texto
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 type ModalLoginProps = {
   isOpenLogin: boolean;
@@ -28,31 +41,34 @@ export const ModalLogin: React.FC<ModalLoginProps> = ({
       <Box sx={{ 
         backgroundColor: "background.paper",
         position: "absolute", 
-        top: {xs: "200px", sm: "20px", md: "80px", xl:"80px"}, 
-        right: {xs: "1.5dvw", sm: "50%", md: "80px", xl:"80px"},
+        top: {xs: "200px", md: heightForModals}, 
+        right: {xs: "1rem", sm:"2rem", md:"4rem", lg:"5rem", xl:"8rem"},
+        width: {xs: "97dvw", sm: "344px"},
+        border: `1px solid ${greyColor[950]}`,
+        borderRadius: "8px",
+        transform: {xs: "translate(50%, 50%)", md: "unset"},
         zIndex: 100,
-        width: {xs: "97dvw", sm: "unset"},
-        transform: {sm: "translate(50%, 50%)", md: "unset"},
+        ...paddingModal
       }}>
+        <Closebutton closeModal={()=>handleClose()} id="bti-close-modal-login" />
         {isOpenForgetPass && 
           <IconButton
             id="bti-back-to-login"
             onClick={() => setIsOpenForgetPass(false)} 
-            sx={{ border: "none", position: "absolute", top: "0.6rem", left: "1rem"}}
+            sx={{ border: "none", position: "absolute", top: "4px", left: "4px"}}
           >
             <ArrowBackOutlinedIcon />
           </IconButton>
         }
         <Heading3 sx={{
-          margin: "1rem auto 0",
+          margin: "1rem auto 24px",
           textAlign: "center",
-
         }}>
-          {isOpenForgetPass ? "Recuperar contraseña" : "Iniciar sesión"}
+          {isOpenForgetPass ? "RECUPERAR CONTRASEÑA" : "INICIAR SESIÓN"}
         </Heading3>  
         {!isOpenForgetPass
-        ? <Login handleClose={handleClose} isModal={true} setIsOpenDrawer={setIsOpenDrawer} setIsOpenForgetPass={setIsOpenForgetPass} />
-        : <ForgetPass handleClose={handleClose} isModal={isOpenForgetPass} setIsOpenDrawer={setIsOpenDrawer} />}
+        ? <Login handleClose={handleClose} isModal={true} setIsOpenDrawer={setIsOpenDrawer} setIsOpenForgetPass={setIsOpenForgetPass} sx={{animation: `0.6s ${fadeInAnimation} ease-out`}} />
+        : <ForgetPass handleClose={handleClose} isModal={isOpenForgetPass} setIsOpenDrawer={setIsOpenDrawer} setIsOpenForgetPass={setIsOpenForgetPass} sx={{animation: `0.6s ${fadeInAnimation} ease-out`}} />}
       </Box>
    </Modal>
   );

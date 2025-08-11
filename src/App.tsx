ae27922eb574f5d81d9987e6e4854be49e080d92
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-11 11:05:41 -0300

    fix App: no se hidrata

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,10 @@ import { MUIThemeProvider } from "./theme/ThemeProvider";
 import { router } from "./routes/AppRoutes";
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
-import { useUserStore } from "@store/useUserStore";
 import { AuthProvider } from "@shared/hooks/AuthProvider";
 import Loading from "@shared/components/Loading";
 
 export const App = () => {
-  const { hasHydrated } = useUserStore();
-
-  // Mostrar loading hasta que el store se haya hidratado
-  if (!hasHydrated) {
-    return <Loading />;
-  }
 
   return (
     <MUIThemeProvider>

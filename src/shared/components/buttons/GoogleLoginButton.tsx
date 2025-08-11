bda4ca797dda4fb050e72eec56ca1d8905c8d031
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-11 16:34:51 -0300

    add: se crea un boton de login con google

---
+++ src/shared/components/buttons/GoogleLoginButton.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { WhiteButton } from "./WhiteButton";
+import GoogleIcon from '@mui/icons-material/Google';
+import { useAuth } from "@/shared/hooks/api/auth/useAuth";
+
+
+interface IGoogleLoginButton {
+  isFetching?: boolean;
+  disabled?: boolean;
+  sx?: object;
+}
+export const GoogleLoginButton: React.FC<IGoogleLoginButton> = ({isFetching = false, disabled = false, sx = {}}) => {
+  const { signInWithGoogle } = useAuth();
+
+  return (
+    <WhiteButton
+      id="bt-header-login-google"
+      text="iniciar con google"
+      isFetching={isFetching}
+      disabled={disabled}
+      icon={<GoogleIcon />}
+      onClick={() => signInWithGoogle()}
+      sx={{ ...sx }}
+    />
+
+  )
+}

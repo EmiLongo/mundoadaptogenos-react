fc85d153332ff719bed7c2890c126ee33540a34a
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-11 21:08:58 -0300

    add LoginWithGoogle: se crea el componente a la izquierda de la pagina de registro

---
+++ src/modules/register/components/LoginWithGoogle.tsx
@@ -0,0 +1,51 @@
+// src/modules/register/components/LoginWithGoogle.tsx
+import React from "react"
+import { GoogleLoginButton } from "@/shared/components/buttons/GoogleLoginButton"
+import { Box } from "@mui/material"
+import floatingMushroomWebp from "@img/home/hero/floating_mushroom.webp";
+import floatingMushroomPng from "@img/home/hero/floating_mushroom.png";
+import shadowMushroomWebp from "@img/home/hero/sombra.webp";
+import shadowMushroomPng from "@img/home/hero/sombra.png";
+
+export const LoginWithGoogle: React.FC = () => {
+  return (
+    <>
+    <Box sx={{
+      display: "flex", 
+      alignItems: "center", 
+      flexDirection: "column",
+      width: {xs: "90%", sm: "550px", md: "383px", lg: "441px", xl: "470px"},
+    }}>
+      <Box
+        component="picture"
+        sx={{ width: "206px", transform: "translateY(-30px)"}}
+      >
+        <source srcSet={floatingMushroomWebp} type="image/webp" />
+        <Box
+          component="img"
+          src={floatingMushroomPng}
+          alt="Hongo Flotando"
+          loading="lazy"
+          decoding="async"
+          width="206px"
+        />
+      </Box>
+      <Box
+        component="picture"
+        sx={{ width: "206px", transform: "translateY(-30px)"}}
+      >
+        <source srcSet={shadowMushroomWebp} type="image/webp" />
+        <Box
+          component="img"
+          src={shadowMushroomPng}
+          alt="Sombra del Hongo"
+          loading="lazy"
+          decoding="async"
+          width="206px"
+        />
+      </Box>
+      <GoogleLoginButton />
+    </Box>
+    </>
+  )
+}

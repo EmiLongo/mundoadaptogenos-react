35b4dcca54fbe2d8b73577d22ddc5762728dce33
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-12 11:35:27 -0300

    md SubproductsMenu: se le agregan estilos al seleccionado, se imposibilita que haga link

--- src/shared/Layout/components/SubproductsMenu.tsx
+++ src/shared/Layout/components/SubproductsMenu.tsx
@@ -4,11 +4,13 @@ import { Box } from "@mui/material"
 import { Heading5 } from "@theme/textStyles"
 import { brownColor, greyColor } from "@theme/theme"
 import { subproductsItems } from "../utils/info"
+import { useLocation } from "react-router-dom"
 
 interface ISubproductsMenu {
   sx?: object;
 }
 export const SubproductsMenu: React.FC<ISubproductsMenu> = ({sx = {}}) => {
+  const { pathname } = useLocation();
   return (
     <Box sx={{
       display: "flex",
@@ -25,26 +27,26 @@ export const SubproductsMenu: React.FC<ISubproductsMenu> = ({sx = {}}) => {
       {subproductsItems.map((item, index) => (
         <Box
         component="a"
-        href={item.path}
+        href={pathname === item.path ? undefined : item.path}
         key={`subproduct-item-${index}`}
         sx={{
           display: "flex",
           alignItems: "center",
           width: "100%",
           height: "42px",
-          textDecoration: "none",
-          color: greyColor[950],
+          textDecoration: pathname === item.path ? "underline" : "none",
+          color: pathname === item.path ? brownColor[800] : greyColor[950],
           cursor: "pointer",
           paddingLeft: "36px",
           "&:hover": {
-            color: brownColor[950],
+            color: pathname === item.path ? brownColor[800] : brownColor[950],
             backgroundColor: greyColor[200],
             textDecoration: "underline",
             transition: "all 0.3s ease-in-out",
           },
         }}
         >
-          <Heading5 sx={{textAlign: "left"}}>{item.text}</Heading5>
+          <Heading5 sx={{textAlign: "left", color: "inherit"}}>{item.text}</Heading5>
         </Box>
       ))}
     </Box>

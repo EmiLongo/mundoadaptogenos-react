7a64b86fd98800e6cb8c482694a505c314677f22
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-12 18:54:04 -0300

    add ShoppingHistoryPage: componente vacío y sus carpetas

---
+++ src/modules/shopping-history/page/ShoppingHistoryPage.tsx
@@ -0,0 +1,8 @@
+// src/modules/shopping-history/page/ShoppingHistory.tsx
+import React from "react"
+
+export const ShoppingHistoryPage: React.FC = () => {
+  return (
+    <div>ShoppingHistory</div>
+  )
+}

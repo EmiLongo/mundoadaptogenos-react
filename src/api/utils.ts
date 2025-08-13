082c0edc6b6563876dc604f800b5339611c73c99
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-13 19:34:49 -0300

    md api/utils: se agrega la url del front

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,6 @@
 export const backendUrl = import.meta.env.VITE_SUPABASE_URL;
 export const backendUrlKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const frontendUrl = import.meta.env.VITE_FRONTEND_URL;
 
 export const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
 export const imagesEndpoint = import.meta.env.VITE_IMAGES_ENDPOINT;

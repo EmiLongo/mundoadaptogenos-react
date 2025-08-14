b60e351aedb3c8c988c80d4adef3bac75369cdd7
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-13 22:36:04 -0300

    md MenuAdmin: estilos

--- src/modules/admin/components/MenuAdmin.tsx
+++ src/modules/admin/components/MenuAdmin.tsx
@@ -8,12 +8,11 @@ import {
 	useMediaQuery,
 	useTheme,
 } from "@mui/material";
-import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
-import { greyColor, brownColor } from "@/theme/theme";
-// import { useUserStore } from "@/store/useUserStore";
+import { greyColor } from "@/theme/theme";
 import { MenuListItem } from "./MenuListItem";
-import { navBarDesktopHeight, navBarMobileHeight } from "@/shared/Layout/utils/info";
-import { MenuOptionsBottom, MenuOptionsTop } from "../utils/info";
+import { navBarDesktopHeight, heightForModalsMobile } from "@shared/Layout/utils/info";
+import { MenuOptionsTop } from "../utils/info";
+import arrowMenuClose from "@img/icons/arrow-menu-close.svg";
 
 
 export const MenuAdmin = () => {
@@ -24,15 +23,14 @@ export const MenuAdmin = () => {
   const [open, setOpen] = useState(isLargeScreen ? true : false);
 
   // const userData = useUserStore((state) => state.user);
-  // const isSuperAdmin = userData?.role === "SUPER_ADMIN";
-  const isSuperAdmin = true;
   
-  const drawerWidthOpen: string = "295px";
+  const drawerWidthOpen: string = "231px";
   const drawerWidthClosed: string = "80px";
-  const drawerHeightOpen: string = isSuperAdmin 
-                                  ? `${MenuOptionsTop.length * 55 + MenuOptionsBottom.length * 55 + 40}px` 
-                                  : `${MenuOptionsTop.length * 55 + 40}px`;
-  const drawerHeightClosed: string = "80px";
+  // const drawerHeightOpen: string = isSuperAdmin 
+  //                                 ? `${MenuOptionsTop.length * 55 + MenuOptionsBottom.length * 55 + 40}px` 
+  //                                 : `${MenuOptionsTop.length * 55 + 40}px`;
+  const drawerHeightOpen: string = `${MenuOptionsTop.length * 55 + 10}px`;
+  const drawerHeightClosed: string = "50px";
   
   const handleDrawerToggle = () => {
     setOpen(!open);
@@ -40,13 +38,17 @@ export const MenuAdmin = () => {
 
   return (
     <>
-    <Box sx={{ height: {xs: navBarMobileHeight, md: 0}, width: {xs: "100%", md: 0}}} />
+    <Box sx={{ 
+      height: {xs: open ? drawerHeightOpen : drawerHeightClosed, md: "100%"}, 
+      width: {xs: "100%", md: open ? drawerWidthOpen : drawerWidthClosed},
+      transition: "all 0.3s ease-in-out",
+    }}/>
     <Box
       sx={{ 
         display: "flex", 
         position: "fixed", 
         left: 0, 
-        top: {xs: navBarMobileHeight, md: navBarDesktopHeight}, 
+        top: {xs: heightForModalsMobile, md: navBarDesktopHeight}, 
         height: {xs: "unset", md: `calc(100vh - ${navBarDesktopHeight})`},
         zIndex: 1000,
       }}
@@ -57,11 +59,13 @@ export const MenuAdmin = () => {
         width: {xs: "100vw", md: open ? drawerWidthOpen : drawerWidthClosed},
         transition: "all 0.3s ease-in-out",
         overflowX: "hidden",
-        backgroundColor: brownColor[200],
+        backgroundColor: greyColor[50],
         color: greyColor[950],
         display: "flex",
         flexDirection: {xs: "row", md: "column"},
-        position: "relative", // Ajustado al contenedor padre
+        position: "relative",
+        borderRight: `1px solid ${greyColor[400]}`,
+        borderBottom: {xs: `1px solid ${greyColor[400]}`, md: "none"},
       }}>
         {/* Botón para abrir/cerrar */}
         <Box
@@ -75,10 +79,11 @@ export const MenuAdmin = () => {
           <IconButton
             id="bti-menu-extend"
             onClick={handleDrawerToggle}
+            sx={{border: "none"}}
           >
-            <ChevronLeftOutlinedIcon sx={{ 
+            <Box component="img" src={arrowMenuClose} sx={{ 
               transform: {xs:open ? "rotate(90deg)" : "rotate(-90deg)", md: open ? "rotate(0deg)" : "rotate(180deg)"},
-              transition: "all 0.3s ease-in-out",
+              transition: "all 0.5s ease-in-out",
             }}/> 
           </IconButton>
         </Box>
@@ -87,13 +92,14 @@ export const MenuAdmin = () => {
           display: "flex",
           flexDirection: {xs: !open ? "row" : "column", md: "column"},
           transition: "all 0.3s ease-in-out",
+          paddingY: {xs: "0px", md: "unset"},
         }}>
           {MenuOptionsTop.length > 0 && MenuOptionsTop.map((item, index) => (
             <MenuListItem key={index} item={item} index={index} currentPath={currentPath} open={open} setOpen={setOpen} />
           ))}
-          {(isSuperAdmin && MenuOptionsBottom.length > 0) && MenuOptionsBottom.map((item, index) => (
+          {/* {(isSuperAdmin && MenuOptionsBottom.length > 0) && MenuOptionsBottom.map((item, index) => (
             <MenuListItem key={index} item={item} index={index} currentPath={currentPath} open={open} setOpen={setOpen} />
-          ))}
+          ))} */}
         </List>
       </Box>
     </Box>

ff04002fde47ccff94a332f36752e763c7a96a34
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-13 22:39:35 -0300

    md ManageProductsPage: se comienza agregando el titulo y las cards sin backend

--- src/modules/admin/page/ManageProductsPage.tsx
+++ src/modules/admin/page/ManageProductsPage.tsx
@@ -1,7 +1,37 @@
+// src/modules/admin/page/ManageProductsPage.tsx
+import { catalogue } from "@/shared/Layout/utils/catalogue"
+import { Box, useMediaQuery, useTheme } from "@mui/material"
 import React from "react"
+import { ProductCard } from "../components/ProductCard"
+import { HeadingPage } from "@/shared/components/HeadingPage"
+import { Heading3, Heading4 } from "@/theme/textStyles"
 
 export const ManageProductsPage: React.FC = () => {
+  const theme = useTheme();
+  const isMoble = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <div>ManageProducts</div>
+    <Box sx={{
+      paddingRight: {xs: "1rem", sm:"2rem", md:"4rem", lg:"5rem", xl:"8rem"},
+      paddingLeft: {xs: "1rem", sm:"2rem", xl:"3rem"},
+      paddingTop: {xs: "24px", md:"48px"},
+    }}>
+      <HeadingPage text="Menu Administrador para gestión de Productos" />
+      {isMoble 
+        ? <Heading4 sx={{textAlign: "center", marginBottom: "16px"}}>PRODUCTOS</Heading4>
+        : <Heading3 sx={{textAlign: "center", marginBottom: "16px"}}>PRODUCTOS</Heading3>
+      }
+      <Box sx={{
+        display: "flex", 
+        flexWrap: "wrap", 
+        gap: {xs: "8px", md: "16px"},
+        justifyContent: "center",
+      }}>
+      {catalogue.map((product) => (
+        <ProductCard key={product.id} product={product} index={product.id} />
+      ))}
+
+      </Box>
+
+    </Box>
   )
 }

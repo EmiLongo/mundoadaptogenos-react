68e7125efa84536d1915f43e2452ba24f554645b
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-15 11:19:06 -0300

    add ProductDetailsPage: pagina basica

---
+++ src/modules/admin/page/ProductDetailsPage.tsx
@@ -0,0 +1,24 @@
+// src/modules/admin/page/ProductDetailsPage.tsx
+import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { HeadingPage } from "@/shared/components/HeadingPage";
+import { Heading3, Heading4 } from "@/theme/textStyles";
+
+export const ProductDetailsPage: React.FC = () => {
+  const theme = useTheme();
+  const isMoble = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box sx={{
+      paddingRight: {xs: "1rem", sm:"2rem", md:"4rem", lg:"5rem", xl:"8rem"},
+      paddingLeft: {xs: "1rem", sm:"2rem", xl:"3rem"},
+      paddingTop: {xs: "24px", md:"48px"},
+      paddingBottom: {xs: "3rem", md:"3rem"},
+    }}>
+      <HeadingPage text="Menu Administrador para gestión de Descuentos" />
+      {isMoble 
+        ? <Heading4 sx={{textAlign: "center", marginBottom: "16px"}}>PRODUCTO</Heading4>
+        : <Heading3 sx={{textAlign: "center", marginBottom: "16px"}}>PRODUCTO</Heading3>
+      }
+    </Box>
+  )
+}

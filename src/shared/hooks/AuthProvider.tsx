655f0449ed9427d90bee810157545272827e9d91
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-19 00:08:47 -0300

    fix AuthProvider: se deja lo necesario para la revalidacion de datos con el backend

--- src/shared/hooks/AuthProvider.tsx
+++ src/shared/hooks/AuthProvider.tsx
@@ -1,89 +1,36 @@
 // src/shared/hooks/AuthProvider.tsx
 import { useEffect } from "react";
 import { useUserStore } from "@/store/useUserStore";
-import { IProfile, IUser } from "@/types/AuthTypes";
+import { IUser, Role } from "@/types/AuthTypes";
 import { supabase } from "@/api/apiClient";
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { setAuth, logout, setLoading } = useUserStore();
+  const { setAuth, logout } = useUserStore();
 
   // Cargar sesión inicial
   useEffect(() => {
-    const initAuth = async () => {
-      setLoading(true);
-
-      // Obtener sesión actual
-      try {
-        const { data: { session }, error } = await supabase.auth.getSession();
-        if (error) {
-          console.error("Error obteniendo sesión:", error);
-          logout();
-          return;
-        }
-        if (session?.user) {
-          console.log("Session found, loading profile...");
-          await loadUserProfile(session.user.id);
-        } else {
-          console.log("No hay sesión activa, cerrando sesión...");
-          logout();
-        }
-      } catch (error) {
-        console.error("Error obteniendo sesión:", error);
-        logout();
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    initAuth();
-
     // Escuchar cambios de autenticación
-    const { data: authListener } = supabase.auth.onAuthStateChange(
+    supabase.auth.onAuthStateChange(
       async (event, session) => {
-        console.log(`Supabase auth event: ${event}`);
+        console.log(`Event: ${event}`);
         if (session?.user) {
-          await loadUserProfile(session.user.id);
+        console.log("user", session?.user);
+        const userData: IUser = {
+          id: session?.user.id,
+          name: session?.user.user_metadata.full_name,
+          email: session?.user.email,
+          phone: session?.user.phone,
+          role: session?.user.app_metadata?.role as Role || Role.CLIENT,
+          created_at: session?.user.created_at,
+        };
+
+        setAuth(userData);
         } else {
           logout();
         }
       }
     );
-
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
   }, []);
-
-  // Función para cargar el perfil desde la DB
-  const loadUserProfile = async (userId: string) => {
-    try {
-      const { data: profile, error } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", userId)
-        .single();
-
-      if (error) {
-        console.error("Error fetching profile:", error);
-        return;
-      }
-
-      if (profile) {
-        const userData: IUser = {
-          id: profile.id,
-          name: profile.full_name,
-          email: profile.email,
-          role: profile.role,
-          avatar_url: profile.avatar_url,
-          created_at: profile.created_at,
-        };
-
-        setAuth(userData, profile as IProfile);
-      }
-    } catch (err) {
-      console.error("Unexpected error loading profile:", err);
-    }
-  };
 
   return children;
 };

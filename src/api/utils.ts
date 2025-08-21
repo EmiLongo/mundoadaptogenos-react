80c78c33fea60e4608cdb6911fa0e0cae6c87582
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-21 13:20:22 -0300

    md types: se agregan datos de cloudinary

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -2,6 +2,5 @@ export const backendUrl = import.meta.env.VITE_SUPABASE_URL;
 export const backendUrlKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const frontendUrl = import.meta.env.VITE_FRONTEND_URL;
 
-export const authEndpoint = import.meta.env.VITE_AUTH_ENDPOINT;
-export const imagesEndpoint = import.meta.env.VITE_IMAGES_ENDPOINT;
-export const userEndpoint = import.meta.env.VITE_USER_ENDPOINT;
+export const cloudinaryCloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const cloudinaryPresetName = import.meta.env.VITE_CLOUDINARY_PRESET_NAME;

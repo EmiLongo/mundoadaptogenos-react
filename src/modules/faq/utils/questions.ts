8a19e4d0bb0705448e75ea7d12a5f883b343ff42
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-08-26 19:27:30 -0300

    add questions: texto de las faq

---
+++ src/modules/faq/utils/questions.ts
@@ -0,0 +1,86 @@
+// src/modules/faq/utils/questions.ts
+export const faqQuestions = [
+  // adaptogenos: 
+  { 
+    sectionTitle: "SOBRE LOS HONGOS ADAPTÓGENOS",
+    questions:
+    [ 
+    {
+      title: "¿Qué son las sustancias Adaptógenas?",
+      description: ["Los adaptógenos son sustancias naturales que se utilizan para ayudar al cuerpo a adaptarse y hacer frente a situaciones de estrés físico, emocional o ambiental. Estas sustancias ayudan a equilibrar el sistema nervioso, hormonal e inmunológico, y pueden mejorar la resistencia y la capacidad de recuperación del cuerpo.",
+      "Están destinados a ayudar a nuestros cuerpos a desarrollar resiliencia al estrés y mitigar sus efectos adicionales en el cuerpo. Cuando se sufre de estrés, el uso de adaptógeno ayuda a que lo percibamos de una manera más ligera y que tengamos una mayor capacidad de lidiar con él, evitando que perdure en el cuerpo.",
+      "Los adaptógenos se han convertido en una opción muy popular para aquellos que buscan formas naturales de mejorar su salud y bienestar e incluyen una amplia variedad de plantas, hierbas y hongos. Estas sustancias se han utilizado tradicionalmente en la medicina natural de diversas culturas."],
+    },
+    {
+      title: "¿Qué requisitos tiene que cumplir?",
+      description: ["Para que una sustancia sea considerada adaptógena tiene que cumplir tres requisitos:",
+        "· Que sea segura y no tóxica: que se pueda consumir de manera indeterminada y que eso no genere ningún tipo de toxicidad.",
+        "· Que genere homeostasis, es decir un estado de equilibrio general y amplio, independientemente del estado de inicio de esa persona. Si una persona tiene mucha energía entonces el adaptógeno ayudará a nivelar para abajo, en cambio, si la persona está con falta de energía o se siente depresiva, la sustancia ayudará a nivelar hacia arriba.",
+        "· El efecto debe ser general y amplio. Tiende a no ser específico en sus propiedades y actúa aumentando la resistencia del organismo en un amplio espectro."
+      ],
+    },
+    {
+      title: "¿Son hongos psicodélicos o mágicos?",
+      description: ["No generan ningún tipo de toxicidad ni tampoco efecto alucinógeno. Los compuestos de estos hongos no alteran la percepción ni causan los conocidos “viajes”."],
+    },
+    {
+      title: "¿Por qué elegir usar adaptógenos?",
+      description: ["La búsqueda de una solución natural y permanente es lo que suele conducir a las personas a elegirlos. Y algo tendrán de especial ya que toda persona que los utiliza comprueba sus propiedades.",
+      "También se utilizan para acompañar los tratamientos alopáticos ya que se pueden tomar en períodos que estemos medicados. Los adaptógenos funcionan mejor junto con una dieta sana y equilibrada y una hidratación adecuada."],
+    },
+    {
+      title: "¿En qué formato los encontramos?",
+      description: ["Recomendamos el doble extracto que es el que mayor materia activa absorbible contiene.",
+        "· Doble extracto",
+        "· Cápsulas",
+        "· Tintura Madre",
+        "· Polvo de Hongo",
+        "· Hongo crudo/ cocido",
+      ],
+    },
+  ],
+  },
+  // dobleExtracto: 
+  { 
+    sectionTitle: "SOBRE EL DOBLE EXTRACTO",
+    questions:[ 
+    {
+      title: "¿Por qué elegir el doble extracto?",
+      description: ["Nuestra fórmula para realizar los extractos es única, logramos su máxima concentración de la materia activa del hongo a partir de los métodos utilizados para la extracción. Hacemos una doble extracción obteniendo más porcentaje de materia activa en estos.",
+        "Además podés elegirnos por la máxima rigurosidad y bioseguridad en todos los procesos y por la excelencia de la materia prima.",
+        "Por eso logramos una fórmula multivalente para que resuelvan diferentes necesidades, variando la dosis y el tiempo de tratamiento, sin ofrecer riesgos para la salud.",
+      ],
+    },
+    {
+      title: "¿Qué dosis y por cuánto tiempo debo usarlo?",
+      description: ["Los tratamientos van a depender de lo que necesite mejorar la persona, del peso y de la edad. Varían de tres a ocho meses y debe ser constante y tomarlo de forma ininterrumpida. Generalmente, los cambios comienzan a notarse a partir de los 20 a 30 días comenzado el tratamiento, pero eso depende de la persona."],
+    },
+    {
+      title: "¿Cómo sé qué hongo necesito?",
+      description: ["Te recomendamos buscar en el menú de nuestro sitio web y podrás leer en detalle todas sus propiedades, beneficios y usos. También podés consultarnos al teléfono o en la web. Nosotros te acompañamos en tu proceso en todo el tratamiento y sanación."],
+    },
+  ],
+  },
+  // envioPago: 
+  { 
+    sectionTitle: "ENVÍO Y PAGO",
+    questions:[
+    {
+      title: "¿Dónde están ubicados? ¿Tienen puntos de retiro?",
+      description: ["Estamos ubicados en la zona sur de la ciudad de Rosario, Santa Fe. Somos una Tienda Online y actualmente no contamos con un local a la calle aunque si tenemos servicio de take away.",
+      "Además de retirarlo por zona sur de Rosario también trabajamos con Correo Argentino, con lo que podemos enviar tu pedido a domicilio (a cualquier lugar de Argentina) o podés elegir retirarlo por cualquier punto de retiro del Correo."],
+    },
+    {
+      title: "¿Envían a mi ciudad?¿Cuánto tiempo tarda?",
+      description: ["Hacemos envíos a todas partes de Argentina.", 
+        "Si vivís en la ciudad de Rosario (Santa Fe) el envío se hace en el día.",
+        "Para el resto del país tardamos de 3 a 5 días hábiles después de procesar tu pedido.",
+      ],
+    },
+    {
+      title: "¿Cómo puedo pagar?",
+      description: ["Podés pagar a través de Mercado Pago, transferencia bancaria o depósito en caso que quieras hacerlo virtual o puede ser en efectivo si te acercás a nuestra ubicación en Rosario."],
+    },
+    ],
+  },
+];

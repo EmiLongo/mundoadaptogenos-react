61802dc3f9c545f130f63682470c88500ae2ef71
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-09-16 14:10:40 -0300

    md ThumbnailImage: se ajusta el tamanio a 250px y hace que se ajuste al container

--- src/shared/components/cloudinary/ThumbnailImage.tsx
+++ src/shared/components/cloudinary/ThumbnailImage.tsx
@@ -1,3 +1,4 @@
+// src/shared/components/cloudinary/ThumbnailImage.tsx
 import React from "react";
 import { AdvancedImage } from "@cloudinary/react";
 import { cld } from "@/api/cloudinaryClient";
@@ -5,22 +6,29 @@ import { fill } from "@cloudinary/url-gen/actions/resize";
 
 interface IThumbnailImage {
   imgPublicId: string;
+  size?: number;
 }
 
-export const ThumbnailImage: React.FC<IThumbnailImage> = ({ imgPublicId }) => {
-  // crear la referencia a la imagen en Cloudinary
+export const ThumbnailImage: React.FC<IThumbnailImage> = ({
+  imgPublicId,
+  size = 250,
+}) => {
   const myImage = cld.image(imgPublicId);
 
-  // aplicamos optimizaciones para thumbnails
+  // thumbnail cuadrado base de Cloudinary
   myImage
     .format("auto")
     .quality("auto")
-    .resize(fill().width(144).height(144)); 
+    .resize(fill().width(size).height(size));
 
   return (
     <AdvancedImage
       cldImg={myImage}
-      styles={{ width: "100%", height: "100%" }}
+      style={{
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+      }}
     />
   );
 };

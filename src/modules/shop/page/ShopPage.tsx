c8fe2dd2cd369c9778272ed1dbe33ca560dca0b3
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-09-26 14:12:51 -0300

    md ShopPage: se agrega h1

--- src/modules/shop/page/ShopPage.tsx
+++ src/modules/shop/page/ShopPage.tsx
@@ -1,8 +1,13 @@
+// src/modules/shop/page/ShopPage.tsx
 import React from "react";
 import { Shop } from "../components/Shop";
+import { HeadingPage } from "@/shared/components/HeadingPage";
 
 export const ShopPage: React.FC = () => {
   return (
+    <>
+    <HeadingPage text="Página con los productos de la tienda" />
     <Shop />
+    </>
   )
 }
